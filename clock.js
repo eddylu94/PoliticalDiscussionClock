abff8b4f029181d2ea38b3b3d8736896b2b6d984
taenin <persons.matthew@gmail.com>
2017-03-01 23:05:55 -0800

    Making CRM great again.

--- clock.js
+++ clock.js
@@ -1,5 +1,9 @@
-﻿getTime();
-setInterval(getTime, 100);
+﻿$(document).ready(() =>{
+	generateClickHandlers();
+	getTime();
+	setInterval(getTime, 100);
+});
+
 
 function getTime() {
 
@@ -29,4 +33,47 @@ function makeNDigits(value, n) {
 		}
 	}
 	return value;
+}
+
+var toggleSubmitFormPopup = function (isOpen) {
+	$("#overlay").toggleClass("visible");
+	$("#submitFormPopup").toggleClass("visible");
+	$("#flavorText").text("");
+    if (!isOpen) {
+        $("#checkBoxButton_yes").html("&#9744; Yes");
+        $("#checkBoxButton_no").html("&#9744; No");
+        $("#submitFormSubmitButton").attr("disabled", true);
+    }
+}
+                
+var toggleCheckBoxes = function (isYes) {
+    $("#submitFormSubmitButton").attr("disabled", false);
+    $("#checkBoxButton_yes").html( isYes ? "&#9745; Yes" : "&#9744; Yes");
+    $("#checkBoxButton_no").html( isYes ? "&#9744; No" : "&#9745; No");
+}
+
+var generateClickHandlers = () =>{
+	$("#checkBoxButton_yes").click((e) =>{
+		toggleCheckBoxes(true);
+	});
+	$("#checkBoxButton_no").click((e) =>{
+		toggleCheckBoxes(false);
+	});
+	$("#resetButton").click((e) =>{
+		toggleSubmitFormPopup(true);
+	});
+	$("#overlay").click((e) =>{
+		toggleSubmitFormPopup(false);
+	});
+	$("#closeButton").click((e) =>{
+		toggleSubmitFormPopup(false);
+	});
+	$("#submitFormSubmitButton").click((e) =>{
+		submitFormSubmit();
+	});
+}
+
+var submitFormSubmit = () =>{
+	var prompts = ["But is it a sandwich?", "Free food in March? Only after 7pm on the 5th floor.", "Make CRM great again!", "Santiago brought up Trump, didn't he?", "I can bench 130lbs."]
+	$("#flavorText").text(prompts[Math.floor(Math.random() * prompts.length)]);
 }
